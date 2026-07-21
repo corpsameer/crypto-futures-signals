d256b1ec09bc4cd736fe38e68efbc336d688e4db
Sameer Agarwal <corporate.sameeragarwal@gmail.com>
2026-07-21 13:43:15 +0530

    Schedule incremental strategy backtests

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('about-crypto-futures-signals', function () {
     $this->info('Crypto Futures Signal Analyzer bootstrap is ready.');
 })->purpose('Display a short project readiness message');
+
+Schedule::command('strategies:backtest --incremental')
+    ->everyTenMinutes()
+    ->withoutOverlapping();
