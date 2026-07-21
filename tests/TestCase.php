1380d36bc6e1359a2014958d1d6d28c847d4e36f
Sameer Agarwal <corporate.sameeragarwal@gmail.com>
2026-07-21 17:36:02 +0530

    Fix recovery hold TP1 final exit PnL

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    protected function createApplication()
+    {
+        return require __DIR__.'/../bootstrap/app.php';
+    }
+}
